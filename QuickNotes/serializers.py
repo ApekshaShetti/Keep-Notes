09afcb9b02d29bd6a715723364b35ee307d6772f
Apeksha <apekshashetti1012@gmail.com>
2023-08-08 21:57:27 +0530

    Added login,logout functionality

--- QuickNotes/serializers.py
+++ QuickNotes/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Note 
+from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 # serializer for a particular model
@@ -7,3 +9,9 @@ class NoteSerializer(ModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
--- QuickNotes/urls.py
+++ QuickNotes/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
-from .views import getRoutes, getNotes, getNote, updateNote, deleteNote, createNote
+from django.urls import path,include
+from .views import getRoutes, getNotes, getNote, updateNote, deleteNote, createNote,register,login
 
 urlpatterns = [
     path('', getRoutes, name="routes"),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
     path('notes/', getNotes, name="notes"),
-     path('notes/create', createNote, name="create-note"),
+    path('notes/create', createNote, name="create-note"),
     path('notes/<str:pk>/update', updateNote, name="update-note"),
     path('notes/<str:pk>/delete', deleteNote, name="delete-note"),
     path('notes/<str:pk>/', getNote, name="note"),
